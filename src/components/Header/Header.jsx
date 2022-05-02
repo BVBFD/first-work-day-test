6e6dbe6add64f353c6e3022f10b91e288f9b9eb8
BVBFD <lsevina126@gmail.com>
2022-05-02 10:29:01 +0900

    first day of working and test git and github

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = (props) => {
+  return (
+    <div className={styles.headerBox}>
+      <span>Header</span>
+    </div>
+  )
+}
+
+export default Header
